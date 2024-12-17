9ab1727230dcae3200570754195085e16f9c389b
04stefke <jovanovics399@yahoo.com>
2024-12-17 18:27:19 +0000

    installed the react-router dom and created the routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Technology from "./pages/Technology.jsx";
+import Crew from "./pages/Crew.jsx";
+import Destination from "./pages/Destination.jsx";
+import Home from "./pages/Home.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/destination",
+		element: <Destination />,
+	},
+	{
+		path: "/crew",
+		element: <Crew />,
+	},
+	{
+		path: "/technology",
+		element: <Technology />,
+	},
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
