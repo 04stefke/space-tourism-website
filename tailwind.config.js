a4fc6105bf4efb98553f4fa0b1c52cfd8ec92fd6
04stefke <jovanovics399@yahoo.com>
2024-12-17 19:50:45 +0000

    created custom boxShadow for the btn on hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			boxShadow: {
+				custom: "0px 0px 15px 20px rgba(0,0,0,0.75)", // Custom shadow
+			},
+		},
+	},
+	plugins: [],
+};
