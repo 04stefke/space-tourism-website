81572c45540dca987486dfda8c3ef3065f3b319d
04stefke <jovanovics399@yahoo.com>
2024-12-17 18:40:33 +0000

    created the app.jsx and navbar changed the paths to be the children of the app.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,24 +5,19 @@ import Technology from "./pages/Technology.jsx";
 import Crew from "./pages/Crew.jsx";
 import Destination from "./pages/Destination.jsx";
 import Home from "./pages/Home.jsx";
+import App from "./App.jsx";
 import "./index.css";
 
 const router = createBrowserRouter([
 	{
 		path: "/",
-		element: <Home />,
-	},
-	{
-		path: "/destination",
-		element: <Destination />,
-	},
-	{
-		path: "/crew",
-		element: <Crew />,
-	},
-	{
-		path: "/technology",
-		element: <Technology />,
+		element: <App />,
+		children: [
+			{ path: "/", element: <Home /> },
+			{ path: "/destination", element: <Destination /> },
+			{ path: "/crew", element: <Crew /> },
+			{ path: "/technology", element: <Technology /> },
+		],
 	},
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
