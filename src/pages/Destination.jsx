7b48bcd993b463cfa54aa5422bb66caab9d6e2cc
04stefke <jovanovics399@yahoo.com>
2024-12-18 13:07:01 +0000

    moved the assets folder to public folder, created map to go through the data

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import data from "../api/data.json";
 const Destination = () => {
-  return (
-    <div>Destination</div>
-  )
-}
+	const destinations = data.destinations;
+	const [selectedDestination, setSelectedDestination] = useState(
+		destinations[0]
+	);
+	return (
+		<section>
+			<article className="uppercase">
+				<h3>
+					<span>01</span>Pick Your Destination
+				</h3>
+				<img
+					src={selectedDestination.images.png}
+					alt={selectedDestination.name}
+				/>
+				{/* Render a list of destinations */}
+				<ul >
+					{destinations.map((destination) => (
+						<li
+							key={destination.name}
+							className={`${
+								destination.name === selectedDestination.name ? "active" : ""
+							}`}
+							onClick={() => setSelectedDestination(destination)}
+						>
+							{destination.name}
+						</li>
+					))}
+				</ul>
+        <div>
+          <h1>{selectedDestination.name}</h1>
+          <p>{selectedDestination.description}</p>
+          <div>
+            <div>
+              <p>AVG. Distance</p>
+              <p>{selectedDestination.distance}</p>
+            </div>
+            <div>
+              <p>EST Travel Time</p>
+              <p>{selectedDestination.travel}</p>
+            </div>
+          </div>
+        </div>
+			</article>
+		</section>
+	);
+};
 
-export default Destination
+export default Destination;
