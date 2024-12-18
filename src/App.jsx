9eb0463e7887751f43104d06084be304ac8dee3e
04stefke <jovanovics399@yahoo.com>
2024-12-18 13:37:43 +0000

    changing positions of the navbar, app.jsx settings

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import Navbar from "./Navbar";
 
 function App() {
 	return (
-		<section className="flex flex-col min-h-screen w-screen items-center gap-10 justify-end pl-[5%] lg:pb-32 pb-10 homeBg">
-			<Navbar/>
-			<Outlet/>
+		<section className="flex flex-col min-h-screen w-screen items-center justify-evenly gap-10 pl-[5%] lg:pb-32 pb-10 homeBg">
+			<Navbar />
+			<div className="w-full min-h-full flex flex-col items-center">
+				<Outlet />
+			</div>
 		</section>
 	);
 }
--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -6,44 +6,43 @@ const Destination = () => {
 		destinations[0]
 	);
 	return (
-		<section>
-			<article className="uppercase">
-				<h3>
-					<span>01</span>Pick Your Destination
-				</h3>
-				<img
-					src={selectedDestination.images.png}
-					alt={selectedDestination.name}
-				/>
-				{/* Render a list of destinations */}
-				<ul >
-					{destinations.map((destination) => (
-						<li
-							key={destination.name}
-							className={`${
-								destination.name === selectedDestination.name ? "active" : ""
-							}`}
-							onClick={() => setSelectedDestination(destination)}
-						>
-							{destination.name}
-						</li>
-					))}
-				</ul>
-        <div>
-          <h1>{selectedDestination.name}</h1>
-          <p>{selectedDestination.description}</p>
-          <div>
-            <div>
-              <p>AVG. Distance</p>
-              <p>{selectedDestination.distance}</p>
-            </div>
-            <div>
-              <p>EST Travel Time</p>
-              <p>{selectedDestination.travel}</p>
-            </div>
-          </div>
-        </div>
-			</article>
+		<section className="uppercase flex items-center justify-end flex-col h-full">
+			<h3>
+				<span>01</span>Pick Your Destination
+			</h3>
+			<img
+				src={selectedDestination.images.png}
+				alt={selectedDestination.name}
+				className="w-44"
+			/>
+			{/* Render a list of destinations */}
+			<ul className="flex items-center justify-center gap-10">
+				{destinations.map((destination) => (
+					<li
+						key={destination.name}
+						className={`${
+							destination.name === selectedDestination.name ? "active" : ""
+						}`}
+						onClick={() => setSelectedDestination(destination)}
+					>
+						{destination.name}
+					</li>
+				))}
+			</ul>
+			<div>
+				<h1>{selectedDestination.name}</h1>
+				<p>{selectedDestination.description}</p>
+				<div>
+					<div>
+						<p>AVG. Distance</p>
+						<p>{selectedDestination.distance}</p>
+					</div>
+					<div>
+						<p>EST Travel Time</p>
+						<p>{selectedDestination.travel}</p>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
