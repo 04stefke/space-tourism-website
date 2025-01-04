c260016a908d9d2fc2832114899d72e0b0202c18
04stefke <jovanovics399@yahoo.com>
2025-01-04 21:36:36 +0000

    made the navbar dropdown on click and fallback

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,35 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "/assets/shared/logo.svg";
+import boxicons from "boxicons";
 import { Link } from "react-router-dom";
 const Navbar = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false)
+
 	return (
-		<header className=" md:flex w-full justify-between items-center lg:pt-10  text-white pt-5 ">
+		<header className="flex w-full justify-between items-center pt-10  text-white">
 			<img src={logo} alt="logo" />
-      
-			<nav className=" h-20 px-28 backdrop-blur-3xl bg-white/[0.02] hidden">
-				<ul className="flex items-center gap-5 lg:gap-10 h-full">
-					<li className="uppercase ">
-						<Link to="/">
-							<span className="invisible lg:visible lg:font-bold lg:pr-2">01</span> Home
-						</Link>
-					</li>
-					<li className="uppercase ">
-						<Link to="/destination">
-							<span className="invisible lg:visible lg:font-bold lg:pr-2">02</span> Destination
-						</Link>
-					</li>
-					<li className="uppercase ">
-						<Link to="/crew">
-							<span className="invisible lg:visible lg:font-bold lg:pr-2">03</span> Crew
-						</Link>
-					</li>
-					<li className="uppercase ">
-						<Link to="/technology">
-							<span className="invisible lg:visible lg:font-bold lg:pr-2">04</span> Technology
-						</Link>
-					</li>
-				</ul>
-			</nav>
+			<ul className={` navbarHamburgerDropdown ${isMenuOpen ? 'right-0' : 'left-[-500px]'}`}>
+				<i className="bx bx-x lg:hidden block cursor-pointer text-5xl self-end" onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
+				<li className="uppercase ">
+					<Link to="/">
+						<span className="lg:font-bold lg:pr-2">01</span> Home
+					</Link>
+				</li>
+				<li className="uppercase ">
+					<Link to="/destination">
+						<span className="lg:font-bold lg:pr-2">02</span> Destination
+					</Link>
+				</li>
+				<li className="uppercase ">
+					<Link to="/crew">
+						<span className="lg:font-bold lg:pr-2">03</span> Crew
+					</Link>
+				</li>
+				<li className="uppercase ">
+					<Link to="/technology">
+						<span className="lg:font-bold lg:pr-2">04</span> Technology
+					</Link>
+				</li>
+			</ul>
+			<i className="bx bx-menu lg:hidden block cursor-pointer text-5xl" onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
+
+
+
 		</header>
 	);
 };
