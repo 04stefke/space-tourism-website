4323096e4a06a6ab29b23ee9c71a1f85db8f5249
04stefke <jovanovics399@yahoo.com>
2025-01-04 22:37:01 +0000

    styled destination for all device sizes

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -6,42 +6,46 @@ const Destination = () => {
 		destinations[0]
 	);
 	return (
-		<section className=" uppercase flex items-center gap-12 flex-col h-full text-white">
-			<h3 className="text-lg">
-				<span className="pr-5">01</span>Pick Your Destination
-			</h3>
-			<img
-				src={selectedDestination.images.png}
-				alt={selectedDestination.name}
-				className="w-44"
-			/>
+		<section className=" uppercase flex items-center justify-center lg:justify-between gap-12 flex-col lg:flex-row h-full text-white">
+			<div className="flex flex-col items-center gap-10 ">
+				<h3 className="text-lg ">
+					<span className="pr-5">01</span>Pick Your Destination
+				</h3>
+				<img
+					src={selectedDestination.images.png}
+					alt={selectedDestination.name}
+					className="w-44 lg:w-80 xl:w-96"
+				/>
+			</div>
 			{/* Render a list of destinations */}
-			<ul className="flex items-center justify-center gap-10">
-				{destinations.map((destination) => (
-					<li
-						key={destination.name}
-						className={`${
-							destination.name === selectedDestination.name ? "active" : ""
-						}`}
-						onClick={() => setSelectedDestination(destination)}
-					>
-						{destination.name}
-					</li>
-				))}
-			</ul>
-			<div className="flex flex-col items-center text-center gap-10 max-w-[70%]">
-				<div className="border-b-2">
-					<h1 className="text-5xl mb-8">{selectedDestination.name}</h1>
-					<p className="text-sm pb-8 ">{selectedDestination.description}</p>
-				</div>
-				<div className=" w-full flex flex-col md:flex-row  items-center justify-center gap-5">
-					<div>
-						<p className="text-sm text-gray-300">AVG. Distance</p>
-						<p className="text-2xl">{selectedDestination.distance}</p>
+			<div className="flex flex-col items-center lg:items-start justify-center lg:max-w-[50%] mt-10">
+				<ul className="flex items-center justify-center lg:items-start gap-5 sm:gap-10">
+					{destinations.map((destination) => (
+						<li
+							key={destination.name}
+							className={`${
+								destination.name === selectedDestination.name ? "active" : ""
+							}`}
+							onClick={() => setSelectedDestination(destination)}
+						>
+							{destination.name}
+						</li>
+					))}
+				</ul>
+				<div className="flex flex-col items-center text-center lg:text-start gap-10 max-w-[70%]">
+					<div className="border-b-2">
+						<h1 className="text-5xl mb-8">{selectedDestination.name}</h1>
+						<p className="text-sm pb-8 ">{selectedDestination.description}</p>
 					</div>
-					<div>
-						<p className="text-sm text-gray-300">EST Travel Time</p>
-						<p className="text-2xl">{selectedDestination.travel}</p>
+					<div className=" w-full flex flex-row items-center justify-center gap-5">
+						<div>
+							<p className="text-sm text-gray-300">AVG. Distance</p>
+							<p className="text-2xl">{selectedDestination.distance}</p>
+						</div>
+						<div>
+							<p className="text-sm text-gray-300">EST Travel Time</p>
+							<p className="text-2xl">{selectedDestination.travel}</p>
+						</div>
 					</div>
 				</div>
 			</div>
