2492d650f5fb761b19746abdf6bd5c460152c833
04stefke <jovanovics399@yahoo.com>
2025-01-11 14:47:12 +0000

    created the structure of the crew page and styling it as supposed to

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Navbar from "./Navbar";
 
 function App() {
 	return (
-		<section className="flex flex-col min-h-screen w-screen items-center md:justify-between gap-10 px-[5%] pb-5 homeBg">
+		<section className="flex flex-col min-h-screen max-h-fit w-screen items-center md:justify-between gap-10 px-[5%] pb-5 homeBg">
 			<Navbar />
+
 			<Outlet />
-			<p className="text-sm hidden md:block ">�� 2023 Space Travel. All rights reserved.</p>
+
+			<p className="text-sm hidden md:block ">
+				�� 2023 Space Travel. All rights reserved.
+			</p>
 		</section>
 	);
 }
--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import data from "../api/data.json";
 const Crew = () => {
-  return (
-    <div>Crew</div>
-  )
-}
+	const crews = data.crew;
+	const [selectedCrew, setSelectedCrew] = useState(crews[0]);
+	return (
+		<section className=" uppercase flex items-center justify-center lg:justify-between gap-12 flex-col lg:flex-row h-screen text-white">
+			<div className=" h-full flex flex-col items-center justify-between">
+				<h3 className="text-lg ">
+					<span className="pr-5">02</span>MEET YOUR CREW
+				</h3>
 
-export default Crew
+				<div className="flex flex-col items-center text-center lg:text-start gap-10 max-w-[70%]">
+					<div>
+						<div>
+							<p>{selectedCrew.role}</p>
+							<h1 className="text-2xl mb-8">{selectedCrew.name}</h1>
+						</div>
+						<p className="text-sm pb-8 capitalize ">{selectedCrew.bio}</p>
+					</div>
+				</div>
+				<ul className="flex items-center justify-center lg:items-start gap-5 sm:gap-10 ">
+					{crews.map((crew) => (
+						<li
+							key={crew.name}
+							className={`cursor-pointer relative`}
+							onClick={() => setSelectedCrew(crew)}
+						>
+							<i
+								class={`${
+									crew.name === selectedCrew.name
+										? "bx bxs-circle"
+										: "bx bx-circle"
+								}`}
+							></i>
+						</li>
+					))}
+				</ul>
+			</div>
+			<img
+				src={selectedCrew.images.webp}
+				alt={selectedCrew.name}
+				className="w-48 lg:w-80 xl:w-96 h-1/2"
+			/>
+		</section>
+	);
+};
+
+export default Crew;
