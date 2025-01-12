27a7196b8a1285308fe376a3a3ce6591a71757ae
04stefke <jovanovics399@yahoo.com>
2025-01-12 11:26:06 +0000

    changed the h-60 to start from sm devices only

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -4,19 +4,19 @@ const Crew = () => {
 	const crews = data.crew;
 	const [selectedCrew, setSelectedCrew] = useState(crews[0]);
 	return (
-		<section className=" uppercase flex items-center justify-center lg:justify-between gap-12 flex-col lg:flex-row h-[60vh] text-white">
+		<section className=" flex items-center justify-center lg:justify-between gap-12 flex-col lg:flex-row sm:h-[60vh] text-white">
 			<div className=" h-full flex flex-col items-center gap-5 justify-between">
-				<h3 className="text-lg  ">
+				<h3 className="text-lg uppercase ">
 					<span className="pr-5">02</span>MEET YOUR CREW
 				</h3>
 
 				<div className="flex flex-col items-center text-center lg:text-start gap-10 max-w-[70%]">
 					<div>
-						<div>
+						<div className="uppercase">
 							<p>{selectedCrew.role}</p>
 							<h1 className="text-2xl mb-8">{selectedCrew.name}</h1>
 						</div>
-						<p className="text-xl pb-8 capitalize lg:max-w-[70%] ">{selectedCrew.bio}</p>
+						<p className="text-lg pb-8 lg:max-w-[70%] ">{selectedCrew.bio}</p>
 					</div>
 				</div>
 				<ul className="flex items-center justify-center lg:items-start gap-5 sm:gap-10 ">
