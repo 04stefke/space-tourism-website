4624fd358fb100935a1c69793949acc85cfbee41
04stefke <jovanovics399@yahoo.com>
2025-01-12 13:24:00 +0000

    created a function for changing the backgrounds on different paths, also changed the tablet pic to start from md size devices

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 
 function App() {
+	const location = useLocation()
+
+	const pageBackgroundSwitcher = (() => {
+		switch (location.pathname) {
+            case "/":
+                return "homeBg";
+            case "/destination":
+                return "destinationBg";
+            case "/crew":
+                return "crewBg";
+            case "/technology":
+                return "technologyBg";
+            default:
+                return "";
+        }
+	})();
 	return (
-		<section className="flex flex-col min-h-screen w-screen items-center md:justify-between gap-10 px-[5%] pb-5 homeBg font-custom">
+		<section className={`${pageBackgroundSwitcher} flex flex-col min-h-screen w-screen items-center md:justify-between gap-10 px-[5%] pb-5 homeBg font-custom transition-all duration-500`}>
 			<Navbar />
 
 			<Outlet />
 
-			<p className="text-sm hidden md:block ">
+			<p className="text-sm hidden md:block text-white ">
 				�� 2023 Space Travel. All rights reserved.
 			</p>
 		</section>
