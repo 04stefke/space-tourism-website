90cd1b8fbc183b05a136e497cea56c1ceeabbbeb
04stefke <jovanovics399@yahoo.com>
2025-01-12 10:23:18 +0000

    finished the responsiveness of the crew page and structured it and styled

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 function App() {
 	return (
-		<section className="flex flex-col min-h-screen max-h-fit w-screen items-center md:justify-between gap-10 px-[5%] pb-5 homeBg">
+		<section className="flex flex-col min-h-screen w-screen items-center md:justify-between gap-10 px-[5%] pb-5 homeBg">
 			<Navbar />
 
 			<Outlet />
