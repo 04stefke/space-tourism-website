fd8bea857da7f50366e74d2eb306cc4bf33c542a
04stefke <jovanovics399@yahoo.com>
2025-01-12 12:59:30 +0000

    added hover effects and active effects in the navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,59 +1,109 @@
 import React, { useState } from "react";
 import logo from "/assets/shared/logo.svg";
 import boxicons from "boxicons";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
-	const [isMenuOpen, setIsMenuOpen] = useState(false)
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	return (
 		<header className="flex w-full justify-between items-center pt-10  text-white">
 			<img src={logo} alt="logo" />
-			<ul className='navbarDefault'>
-				<li className="uppercase ">
-					<Link to="/">
+			<ul className="navbarDefault">
+				<li className="uppercase relative ">
+					<NavLink
+						className={({ isActive }) =>
+							` ${isActive ? "navbarActive" : "text-gray-400"}`
+						}
+						to="/"
+					>
 						<span className="lg:font-bold lg:pr-2">00</span> Home
-					</Link>
+					</NavLink>
 				</li>
-				<li className="uppercase ">
-					<Link to="/destination">
+				<li className="uppercase relative ">
+					<NavLink
+						className={({ isActive }) =>
+							` ${isActive ? "navbarActive" : "text-gray-400"}`
+						}
+						to="/destination"
+					>
 						<span className="lg:font-bold lg:pr-2">01</span> Destination
-					</Link>
+					</NavLink>
 				</li>
-				<li className="uppercase ">
-					<Link to="/crew">
+				<li className="uppercase relative ">
+					<NavLink
+						className={({ isActive }) =>
+							` ${isActive ? "navbarActive" : "text-gray-400"}`
+						}
+						to="/crew"
+					>
 						<span className="lg:font-bold lg:pr-2">02</span> Crew
-					</Link>
+					</NavLink>
 				</li>
-				<li className="uppercase ">
-					<Link to="/technology">
+				<li className="uppercase relative ">
+					<NavLink
+						className={({ isActive }) =>
+							` ${isActive ? "navbarActive" : "text-gray-400"}`
+						}
+						to="/technology"
+					>
 						<span className="lg:font-bold lg:pr-2">03</span> Technology
-					</Link>
+					</NavLink>
 				</li>
 			</ul>
-			<ul className={` navbarHamburgerDropdown ${isMenuOpen ? 'right-0' : 'left-[-500px]'}`} >
-				<i className="bx bx-x lg:hidden block cursor-pointer text-5xl self-end mb-10" onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
-				<li className="uppercase ">
-					<Link to="/">
+			<ul
+				className={` navbarHamburgerDropdown ${
+					isMenuOpen ? "right-0" : "left-[-500px]"
+				}`}
+			>
+				<i
+					className="bx bx-x lg:hidden block cursor-pointer text-5xl self-end mb-10"
+					onClick={() => setIsMenuOpen(!isMenuOpen)}
+				></i>
+				<li className="uppercase relative ">
+					<NavLink
+						className={({ isActive }) =>
+							` ${isActive ? "navbarSmallActive" : "text-gray-200"}`
+						}
+						to="/"
+					>
 						<span className="font-bold pr-2">00</span> Home
-					</Link>
+					</NavLink>
 				</li>
-				<li className="uppercase ">
-					<Link to="/destination">
+				<li className="uppercase relative ">
+					<NavLink
+						className={({ isActive }) =>
+							` ${isActive ? "navbarSmallActive" : "text-gray-200"}`
+						}
+						to="/destination"
+					>
 						<span className="font-bold pr-2">01</span> Destination
-					</Link>
+					</NavLink>
 				</li>
-				<li className="uppercase ">
-					<Link to="/crew">
+				<li className="uppercase relative ">
+					<NavLink
+						className={({ isActive }) =>
+							` ${isActive ? "navbarSmallActive" : "text-gray-200"}`
+						}
+						to="/crew"
+					>
 						<span className="font-bold pr-2">02</span> Crew
-					</Link>
+					</NavLink>
 				</li>
-				<li className="uppercase ">
-					<Link to="/technology">
+				<li className="uppercase relative ">
+					<NavLink
+						className={({ isActive }) =>
+							` ${isActive ? "navbarSmallActive" : "text-gray-200"}`
+						}
+						to="/technology"
+					>
 						<span className="font-bold pr-2">03</span> Technology
-					</Link>
+					</NavLink>
 				</li>
 			</ul>
-			<i className="bx bx-menu lg:hidden block cursor-pointer text-5xl" onClick={() => setIsMenuOpen(!isMenuOpen)}></i>
+			<i
+				className="bx bx-menu lg:hidden block cursor-pointer text-5xl"
+				onClick={() => setIsMenuOpen(!isMenuOpen)}
+			></i>
 		</header>
 	);
 };
